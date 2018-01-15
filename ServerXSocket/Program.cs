c8131bcff582e06c7d2346d70991366a3e54c1d7
Mateusz Stanik <matstanik@gmail.com>
2018-01-15 18:12:34 +0100

    Connection between clint and server

---
+++ ServerXSocket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using XSockets.Core.Common.Socket;
+using XSockets.Core.XSocket;
+using XSockets.Core.XSocket.Helpers;
+using XSockets.Plugin.Framework;
+using XSockets.Plugin.Framework.Attributes;
+
+namespace ServerXSocket
+{
+
+    public class Generic : XSocketController
+    {
+        public async Task CallAllClients()
+        {
+            await this.InvokeToAll("test");
+        }
+    }
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            using (var container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>())
+            {
+                container.Start();
+                Console.WriteLine("Serwer started!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- TestXSocket/Program.cs
+++ TestXSocket/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Topshelf;
 using XSockets;
 using XSockets.Core.Common.Socket;
+using XSockets.Core.Utility.MessageQueue.Interceptors;
 using XSockets.Core.XSocket;
 using XSockets.Core.XSocket.Helpers;
 using XSockets.Plugin.Framework;
@@ -15,53 +18,23 @@ using XSockets.Plugin.Framework.Attributes;
 namespace TestXSocket
 {
 
-    class Program
-    {
-        //[XSocketMetadata(PluginAlias = "chat")]
-        public class Chat : XSocketController
-        {
-            public void ChatMessage1(string message)
-            {
-                this.InvokeToAll(message, "ChatMessage");
-            }
-            public void ChatMessage(string message)
-            {
-                this.InvokeToAll(new { Text = "dupka" }, "chatmessage");
-            }
-        }
+    class  main{
         static void Main(string[] args)
         {
+            //System.Threading.Thread.Sleep(3000);
+            var c = new XSocketClient("ws://127.0.0.1:4502", "http://localhost", "generic");
+            
+            c.OnConnected += (sender, eventArgs) => Console.WriteLine("Connected");
+            c.Controller("generic").OnOpen += (sender, connectArgs) => Console.WriteLine("Generic Open");
 
-
-            using (var container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>())
-            {
-                container.Start();
-                foreach (var server in container.Servers)
-                {
-                    Console.WriteLine(server.ConfigurationSetting.Endpoint);
-                }
-                try
-                {
-                    var conn = new XSocketClient("ws://127.0.0.1:4502", "http://localhost", "chat");
-                    conn.Open();
-
-                    conn.Controller("chat").On<dynamic>("chatmessage", data => Console.WriteLine(data.Text));
-
-
-                    //conn.Controller("chat").OnOpen += (sender, connectArgs) => {
-                    //    Console.WriteLine("Open {0}", connectArgs.ClientInfo.Controller);
-                    //};
-                    conn.Controller("chat").On<string>("ChatMessage", data => Console.WriteLine(data));
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                
-                Console.WriteLine("Server started, hit enter to quit");
-                Console.ReadLine();
-            }
+            c.Open();
+            c.Controller("generic").Invoke("CallAllClients");
+            c.Controller("generic").On("test", () => Console.WriteLine("Syntaxerror did it!!! "));
+            Console.ReadLine();
         }
+
+      
     }
+    
 
 }
